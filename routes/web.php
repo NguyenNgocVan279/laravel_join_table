b09737fa8bee16bab75d26b3fcff72606b0bf30e
NguyenNgocVan279 <nguyenngocvan279@gmail.com>
2022-01-24 09:20:04 +0700

    Laravel Example of Join Multiple Tables

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\JointableController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('join_table', [JointableController::class, 'index']);
